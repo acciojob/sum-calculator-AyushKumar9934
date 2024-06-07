386c7dceffa57a019eb89576f509c371dc8321d6
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-06-07 10:48:21 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [sumtotal,setSumTotal]=useState(0);
+  const [number,setNumber]=useState([]);
+  function handleChange(){
+    
+  }
+  useEffect(()=>{
+    setSumTotal((number.reduce((a,b)=>a+b,0)))
+
+  },[number])
+function handleChange(value){
+setNumber(prevNumber=>[...prevNumber,parseInt(value,10)])
+  
+}
   return (
     <div>
         {/* Do not remove the main div */}
+        <input onChange={(e)=>handleChange(e.target.value)} type="number"></input>
+        <p>Sum: {sumtotal}</p>
     </div>
   )
 }
